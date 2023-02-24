667bc1eb389e9a5f22db1514270d0673b442ee0c
Ksenia91Dumina <100465785+Ksenia91Dumina@users.noreply.github.com>
2023-02-24 12:15:19 +0300

    add-stats-service

--- stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class StatsClient extends BaseClient {
-    public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
+    public StatsClient(@Value("${stats-service.url}") String serverUrl, RestTemplateBuilder builder) {
         super(builder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new)
